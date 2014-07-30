026c78473d80177a911bfa1e9dab9dcfbc3b70be
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-07-30 18:46:58 -0500

    yes

--- week-3/3-js-variables-objects/my_solution.js
+++ week-3/3-js-variables-objects/my_solution.js
@@ -1,24 +1,21 @@
-// I paired [by myself, with:] on this challenge.
+// I paired with Rob Yang on this challenge.
 
 // __________________________________________
 // Write your code below.
 
-
-
-
-
-
-
+var secretNumber = 7;
+var password = "just open the door"
+var allowedIn = false;
+var members = ["John", "Rob", "Pablo", "Mary"];
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
-// 
-// 
-
+// // I learned the syntax for an array, that will be useful going forward.
+// We ran into almost 0 problems with this project. 
+// I am very confident with the learning competencies
+// I enjoyed working with my partner, learning a bit about every person I have paired with as part of the pre-work questions 
+// has been a very entertaining portion of this course. 
+// There were minimal problems in this challenge, it only took 26 minutes :P
 
 // __________________________________________
 // Test Code:  Do not alter code below this line.
@@ -85,4 +82,3 @@ assert(
   "The fourth element in the value of members should be 'Mary'.",
   "9. "
 )
-
