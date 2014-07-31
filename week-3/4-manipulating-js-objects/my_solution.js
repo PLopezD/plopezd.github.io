d1925896663471b40abb8872d059b73339b8bb35
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-07-30 22:49:45 -0500

    yes

--- week-3/4-manipulating-js-objects/my_solution.js
+++ week-3/4-manipulating-js-objects/my_solution.js
@@ -1,9 +1,10 @@
-// I worked on this challenge: [by myself, with: ]
+  // I worked on this challenge by myself.
 
 
-// There is a section below where you will write your code.
+//     There is a section below where you will write your code.
 // Do not alter this object here.
 
+// 
 
 var terah = {
   name: "Terah",
@@ -19,22 +20,58 @@ var terah = {
 // __________________________________________
 // Write your code below.
 
+//defines the object 'adam'
+var adam = {};
 
+// adds name property to adam
+adam.name = "Adam";
 
+//adds spouse property to terah
+terah.spouse = adam;
 
+//edit terah's weight property
+terah.weight = 125;
 
+//removes terah's eye color property
+delete terah.eyeColor;
 
+//add spouse property to adam
+adam.spouse = terah;
 
+// defines and adds children property to terah  object
+terah.children = {};
 
-// __________________________________________
+// creates carson, colton and carter objects
+var carson = {};
+var colton = {};
+var carter = {};
+
+//make carson.name property equal to string 'Carson'
+carson.name = "Carson";
+
+//add carson to terah's children property (how can I add all the kids at once)
+// some type of - children(carson,carter,colton) = {carson,carter,colton}
+terah.children.carson = carson;
+
+//make carter.name property equal to string 'Carter'
+carter.name = "Carter";
+
+//add carter to terah children property
+terah.children.carter = carter;
+
+//make colton.name property equal to string 'Colton'
+colton.name = "Colton";
+
+//add colton to terah children property
+terah.children.colton = colton;
+
+//add children property to adam equal to terah.children
+adam.children = terah.children;
 // Reflection: Use the reflection guidelines
-// 
-// 
-// 
-// 
-// 
-// 
-
+  // This project was pretty interesting, learned a lot about syntax and overwriting variables.
+  // I was a bit irritated that I could not find a way to add all the children variables at once (atleast not in a timely manner).
+  // If anyone knows and could share in my issues that would be appreciated. I'm afraid I may have confused object and property in some 
+  // of my pseudocode. 
 
 // __________________________________________
 // Driver Code:  Do not alter code below this line.
@@ -59,11 +96,13 @@ assert(
   "2. "
 )
 
+
 assert(
   terah.spouse === adam,
   "terah should have a spouse property with the value of the variable adam.",
   "3. "
 )
+
 
 assert(
   terah.weight === 125,
@@ -71,11 +110,13 @@ assert(
   "4. "
 )
 
+
 assert(
   terah.eyeColor === undefined,
   "The terah eyeColor property should be removed.",
   "5. "
 )
+
 
 assert(
   terah.spouse.spouse === terah,
@@ -88,6 +129,14 @@ assert(
   "The value of the terah children property should be an Object.",
   "7. "
 )
+
+assert(
+  terah.children.carson.name === "Carson",
+  "The terah children property should have a carson property with its own property name with a value of 'Carson'.",
+  "8. "
+)
+
+
 
 assert(
   terah.children.carson.name === "Carson",
