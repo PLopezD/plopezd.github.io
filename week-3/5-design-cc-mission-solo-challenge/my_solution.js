f54267e7d0b9db2192e6fed23ebc45c5242ec9ca
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-08-04 12:12:16 -0500

    yes

--- week-3/5-design-cc-mission-solo-challenge/my_solution.js
+++ week-3/5-design-cc-mission-solo-challenge/my_solution.js
@@ -1,9 +1,17 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
-// 
+// Main Character: 
+// 		name: Jedi Pablo 
+// 		Class: Jedi
+// 		Weapon: Lightsaber
+// 		Leap Strength: 50
+// Enemy: 
+// 		name: Dagorey
+// 		Class: sith
+// 		weapon: machete 
+// 		leap strength: 25
+//  	lightning: 25
 
 // Pseudocode
 // 
