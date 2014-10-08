83f611b648feb3790d6a1b1201289b8612344636
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-10-08 08:37:11 -0400

    lol

---
+++ phase2/notes/monday oct 6.rb
@@ -0,0 +1,22 @@
+ Open Systems Interconnection (OSI)
+-- People dont need to see paula abdul -- bottom to top - 
+applcation }  	-   
+presentation }
+session }
+
+
+transport
+network
+data link
+physical
+
+
+
+
+
+
+
+
+
+
+
