7fa9e3c5138038d8f87738e135cb5684f9a84535
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-10-27 23:03:12 -0400

    added image to scrape post

--- thequickscrape.rb
+++ thequickscrape.rb
@@ -1,1 +1,9 @@
-the quick scrape 
+require 'nokogiri'
+doc = Nokogiri::HTML(File.open('clisttest.html'))
+
+
+
+text_array = []
+doc.search('.txt').map { |text|  text_array.push(text.inner_text)}
+p text_array
+
