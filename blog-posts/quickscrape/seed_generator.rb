cd09ac5ac84a1a353b8ad5c33779ba772313b405
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-12-06 16:05:33 -0600

    fixed problem with media queries

---
+++ blog-posts/quickscrape/seed_generator.rb
@@ -0,0 +1,19 @@
+# # jtbd: generate sample  records for app
+
+#users 
+# 	-id
+# 	-name 
+
+#articles
+# 	-id
+# 	-author
+# 	-body 
+
+
+#comments
+# 	- id
+# 	- author
+# 	- body
+# 	- article
+
+
