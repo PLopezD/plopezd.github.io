5bf55d0dc5db8a78f01e37f7f2a25c3e2344e9af
Pablo Lopez Domowicz <pablolopezdomowicz@Pablos-MacBook-Pro.local>
2015-01-29 10:19:05 -0600

    added php script

--- thequickscrape.rb
+++ thequickscrape.rb
@@ -1,7 +1,6 @@
 require 'nokogiri'
-doc = Nokogiri::HTML(File.open('clisttest.html'))
-
-
+# doc = Nokogiri::HTML(File.open('clisttest.html'))
+doc = Nokogiri::HTML('http://chicago.craigslist.org/')
 
 text_array = []
 doc.search('.txt').map { |text|  text_array.push(text.inner_text)}
