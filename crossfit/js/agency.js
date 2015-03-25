e9905aa57e0973705f78447b3f8748e4df1ea7f3
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2015-03-25 18:01:56 -0500

    added crossfit attempt#1

---
+++ crossfit/js/agency.js
@@ -0,0 +1,66 @@
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+	target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+	$('.navbar-toggle:visible').click();
+});
+var map;
+var infowindow;
+
+function initialize() {
+  var austin = new google.maps.LatLng(30.273332, -97.744384);
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: austin,
+    zoom: 15
+  });
+
+  var request = {
+    location: austin,
+    radius: 5000,
+    keyword: ['crossfit']
+  };
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    console.log(place)
+    var enter = "\r\n"
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
