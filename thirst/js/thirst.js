0c249c08d8a0f5c02187fd15c40da589446d653e
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2015-03-31 12:17:37 -0500

    added listeners to search and dropdown

--- thirst/js/thirst.js
+++ thirst/js/thirst.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 	console.log('Ready!')
 
+	$('.sort').change(function(e) {
+		e.preventDefault()
+		console.log("sort change")
+	});
+	
+	$('.search').submit(function(e) {
+		e.preventDefault()
+		console.log("search submit")
+	});
+
 });
