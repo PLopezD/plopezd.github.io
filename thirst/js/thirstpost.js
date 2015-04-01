e20fcb179bdbbe20bda8a381cc3ad7a55701cd1d
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2015-04-01 17:22:26 -0500

    added js to update dom on resize

--- thirst/js/thirstpost.js
+++ thirst/js/thirstpost.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-	$( window ).resize(function() {
 		var winWidth = $( window ).width() 
-		if (winWidth < 766){
-			$("#logo").src = "../img/white.png"
+	if(winWidth < 768){
+		$("#logo")[0].src = "../img/white.png"
+		$("#logo").addClass('pink')
+		$(".showbox").hide()
+	};
+
+	$( window ).resize(function() {
+		var resizeWidth = $( window ).width() 
+		if (resizeWidth < 768){
+			$("#logo")[0].src = "../img/white.png"
 			$("#logo").addClass('pink')
+			$(".showbox").hide()
 		} else {
+			$("#logo")[0].src = "../img/multi.png"
 			$("#logo").removeClass('pink')
-			console.log("not")
+			$(".showbox").show()
 		}
 
 	});
 
-	
 
 });
