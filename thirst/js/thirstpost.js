e0df774e05282444c5ceb3f408168f4fcb73cceb
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2015-04-02 12:01:42 -0500

    resize color fix

--- thirst/js/thirstpost.js
+++ thirst/js/thirstpost.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 		var winWidth = $( window ).width() 
 	if(winWidth < 768){
 		$("#logo")[0].src = "../img/white.png"
-		$("#logo").addClass('pink')
+		$("#logo").addClass('purple')
 		$(".showbox").hide()
 	};
 
@@ -10,11 +10,11 @@ $(document).ready(function() {
 		var resizeWidth = $( window ).width() 
 		if (resizeWidth < 768){
 			$("#logo")[0].src = "../img/white.png"
-			$("#logo").addClass('pink')
+			$("#logo").addClass('purple')
 			$(".showbox").hide()
 		} else {
 			$("#logo")[0].src = "../img/multi.png"
-			$("#logo").removeClass('pink')
+			$("#logo").removeClass('purple')
 			$(".showbox").show()
 		}
 
